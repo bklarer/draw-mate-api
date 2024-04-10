d972f5800385f27c4cae992c2c57eda250abc21a
Benjamin <benjaminklarer@gmail.com>
2024-04-09 23:43:04 -0700

    Setup participant

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :events
+    has_many :participants
     
     validates :first_name, presence: true
     validates :last_name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_10_061622) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_10_063624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_10_061622) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "participants", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_participants_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -31,4 +41,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_10_061622) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "participants", "users"
 end
